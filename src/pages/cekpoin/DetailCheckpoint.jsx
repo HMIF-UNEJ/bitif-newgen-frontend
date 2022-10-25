ed470e379428386c88817552c31724c6473fdaff
Heri Setyawan <herisetyawan233@gmail.com>
2022-10-25 22:20:15 +0700

    fixing some page

--- src/pages/cekpoin/DetailCheckpoint.jsx
+++ src/pages/cekpoin/DetailCheckpoint.jsx
@@ -2,10 +2,11 @@ import React from "react"
 import PartCekPoint from "../beranda/parts/CekPoint"
 import Biodata from "./part/Biodata"
 import TablePoint from "./part/TablePoint"
+import bgBlue from '../../assets/image/bgBlue.svg';
 export default function DetailPoint(){
     return (
         <div id='custom-all-panels' className='overflow-x-hidden'>
-            <section className="bg-blue-500">
+            <section className="bg-blue-500" style={{ backgroundImage: `url(${bgBlue})` }}>
                 <PartCekPoint />
             </section>
             <section>
--- src/pages/struktur/CommitteeStructure.jsx
+++ src/pages/struktur/CommitteeStructure.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Jumbotron from "./part/Jumbotron";
 import PartStruktur from "./part/Struktur";
-
+import bgGreen from "../../assets/image/bgGreen.svg"
 function Struktur(){
 
     return (
         <>
             <div id='custom-all-panels' className='overflow-x-hidden'>
-                <section className="first bg-green-500 h-[50vh]">
+                <section className="first bg-green-500 h-[50vh]" style={{ backgroundImage: `url(${bgGreen})` }}>
                     <Jumbotron/>
                 </section>
                 <section className="">
